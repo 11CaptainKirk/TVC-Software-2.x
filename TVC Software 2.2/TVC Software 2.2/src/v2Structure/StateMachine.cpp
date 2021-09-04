c029ebd7ea320d08356974138767ee2b6d23cd36
Robert Kirk <robertkirk@Roberts-MacBook-Air.local>
2021-09-04 00:50:46 -0400

    Start of new framework

---
+++ TVC Software 2.2/TVC Software 2.2/src/v2Structure/StateMachine.cpp
@@ -0,0 +1,18 @@
+
+void stateMachine(int systemState)
+{
+    switch (systemState)
+    {
+    case 0:
+        // Initializing ( Not Ready )
+        break;
+    case 1:
+        // Ground Idle ( Ready For Flight )
+        break;
+    case 2:
+        // Ground Idle ( Ready For Flight )
+        break;
+    default:
+        // code block
+    }
+}
